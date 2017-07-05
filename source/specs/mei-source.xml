music-encoding/music-encoding

commit 4edb457585094c66c66779d3b555c7932172447c

Make <head> available in more places within <meiHead>
add <head> to structured content models

Fixes #444

source/specs/mei-source.xml
@@ -13265,6 +13265,9 @@
         <memberOf key="model.pubStmtPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:ref name="macro.availabilityPart"/>
       </content>
       <remarks>
@@ -14725,6 +14728,9 @@
         <memberOf key="model.editorialDeclPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="model.pLike"/>
         </rng:oneOrMore>
@@ -15508,6 +15514,9 @@
         <memberOf key="att.lang"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="model.editionLike"/>
           <rng:zeroOrMore>
@@ -15587,6 +15596,9 @@
         <memberOf key="model.headerPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:optional>
           <rng:ref name="appInfo"/>
         </rng:optional>
@@ -15845,6 +15857,9 @@
         <memberOf key="model.expressionLike"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:zeroOrMore>
           <rng:ref name="model.identifierLike"/>
         </rng:zeroOrMore>
@@ -17282,6 +17297,9 @@
       </classes>
       <content>
         <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
+        <rng:zeroOrMore>
           <rng:choice>
             <rng:ref name="model.pLike"/>
             <rng:ref name="model.lgLike"/>
@@ -17368,6 +17386,9 @@
         <memberOf key="model.itemLike"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:zeroOrMore>
           <rng:ref name="model.identifierLike"/>
         </rng:zeroOrMore>
@@ -18898,6 +18919,9 @@
         <memberOf key="model.editorialDeclPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="model.pLike"/>
         </rng:oneOrMore>
@@ -19845,29 +19869,27 @@
           they are required by software, or 2) multiple, alternative slurs are needed.</p>
       </remarks>
     </elementSpec>
-    <elementSpec ident="physDesc" module="MEI.header">
-      <desc>(physical description) – Container for information about the appearance, construction,
-        or handling of physical materials, such as their dimension, quantity, color, style, and
-        technique of creation.</desc>
+    <elementSpec ident="normalization" module="MEI.header">
+      <desc>Indicates the extent of normalization or regularization of the original source carried
+        out in converting it to electronic form.</desc>
       <classes>
         <memberOf key="att.common"/>
         <memberOf key="att.bibl"/>
+        <memberOf key="att.dataPointing"/>
+        <memberOf key="att.lang"/>
+        <memberOf key="att.regularMethod"/>
+        <memberOf key="model.editorialDeclPart"/>
       </classes>
       <content>
         <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
+        <rng:oneOrMore>
           <rng:ref name="model.pLike"/>
-        </rng:zeroOrMore>
-        <rng:zeroOrMore>
-          <rng:ref name="model.physDescPart"/>
-        </rng:zeroOrMore>
+        </rng:oneOrMore>
       </content>
       <remarks>
-        <p>Dedicatory text and title page features may also be encoded here when they are not
-          transcribed as part of the front or back matter; i.e., when they are considered to be
-          meta-data rather than a transcription.</p>
-      </remarks>
-      <remarks>
-        <p>This element is modelled on an element in the Encoded Archival Description (EAD)
+        <p>This element is modelled on an element in the Text Encoding Initiative (TEI)
           standard.</p>
       </remarks>
     </elementSpec>
@@ -19883,6 +19905,9 @@
       </classes>
       <content>
         <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
+        <rng:zeroOrMore>
           <rng:group>
             <rng:ref name="model.repositoryLike"/>
             <rng:zeroOrMore>
@@ -19894,11 +19919,6 @@
           <rng:ref name="provenance"/>
         </rng:optional>
       </content>
-      <remarks>
-        <p>Multiple <gi scheme="TEI">repository</gi> child elements indicate the locations of
-          separated parts of a single intellectual entity. Repeat <gi scheme="MEI">physLoc</gi> to
-          record multiple locations of complete items.</p>
-      </remarks>
       <remarks>
         <p>This element is modelled on an element in the Encoded Archival Description (EAD)
           standard.</p>
@@ -20263,6 +20283,9 @@
         <memberOf key="att.bibl"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:choice>
           <rng:optional>
             <rng:ref name="unpub"/>
@@ -20860,6 +20883,9 @@
         <memberOf key="model.respLike"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:choice>
           <rng:zeroOrMore>
             <rng:choice>
@@ -20981,6 +21007,9 @@
         <memberOf key="att.bibl"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="change"/>
         </rng:oneOrMore>
@@ -21291,6 +21320,9 @@
         <memberOf key="model.editorialDeclPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="model.pLike"/>
         </rng:oneOrMore>
@@ -21358,6 +21390,9 @@
         <memberOf key="att.bibl"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="model.titleLike"/>
         </rng:oneOrMore>
@@ -21565,6 +21600,9 @@
         <memberOf key="model.manifestationLike"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:zeroOrMore>
           <rng:ref name="model.identifierLike"/>
         </rng:zeroOrMore>
@@ -22175,6 +22213,9 @@
         <memberOf key="model.editorialDeclPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="model.pLike"/>
         </rng:oneOrMore>
@@ -22557,6 +22598,9 @@
         <memberOf key="att.common"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:optional>
           <rng:ref name="desc"/>
         </rng:optional>
@@ -22631,6 +22675,9 @@
         <memberOf key="att.common"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:optional>
           <rng:ref name="model.biblLike"/>
         </rng:optional>
@@ -23018,6 +23065,9 @@
         <memberOf key="model.frontPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:choice>
             <rng:ref name="model.figureLike"/>
@@ -23098,6 +23148,9 @@
         <memberOf key="att.common"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="model.titleLike"/>
         </rng:oneOrMore>
@@ -23734,6 +23787,9 @@
       </classes>
       <content>
         <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
+        <rng:zeroOrMore>
           <rng:ref name="model.identifierLike"/>
         </rng:zeroOrMore>
         <rng:optional>
@@ -23804,6 +23860,9 @@
         <memberOf key="model.headerPart"/>
       </classes>
       <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
         <rng:oneOrMore>
           <rng:ref name="work"/>
         </rng:oneOrMore>

commit 02172a74e6bcf2d95708bbec7757b8b0e2fb4420

Fix errors introduced by 4edb457

source/specs/mei-source.xml
@@ -18907,30 +18907,6 @@
         </rng:zeroOrMore>
       </content>
     </elementSpec>
-    <elementSpec ident="normalization" module="MEI.header">
-      <desc>Indicates the extent of normalization or regularization of the original source carried
-        out in converting it to electronic form.</desc>
-      <classes>
-        <memberOf key="att.common"/>
-        <memberOf key="att.bibl"/>
-        <memberOf key="att.dataPointing"/>
-        <memberOf key="att.lang"/>
-        <memberOf key="att.regularMethod"/>
-        <memberOf key="model.editorialDeclPart"/>
-      </classes>
-      <content>
-        <rng:zeroOrMore>
-          <rng:ref name="model.headLike"/>
-        </rng:zeroOrMore>
-        <rng:oneOrMore>
-          <rng:ref name="model.pLike"/>
-        </rng:oneOrMore>
-      </content>
-      <remarks>
-        <p>This element is modelled on an element in the Text Encoding Initiative (TEI)
-          standard.</p>
-      </remarks>
-    </elementSpec>
     <elementSpec ident="nameLink" module="MEI.namesdates">
       <desc>(name link) – Contains a connecting phrase or link used within a name but not regarded
         as part of it, such as "van der" or "of", "from", etc.</desc>
@@ -18952,6 +18928,30 @@
             <rng:ref name="model.transcriptionLike"/>
           </rng:choice>
         </rng:zeroOrMore>
+      </content>
+      <remarks>
+        <p>This element is modelled on an element in the Text Encoding Initiative (TEI)
+          standard.</p>
+      </remarks>
+    </elementSpec>
+    <elementSpec ident="normalization" module="MEI.header">
+      <desc>Indicates the extent of normalization or regularization of the original source carried
+        out in converting it to electronic form.</desc>
+      <classes>
+        <memberOf key="att.common"/>
+        <memberOf key="att.bibl"/>
+        <memberOf key="att.dataPointing"/>
+        <memberOf key="att.lang"/>
+        <memberOf key="att.regularMethod"/>
+        <memberOf key="model.editorialDeclPart"/>
+      </classes>
+      <content>
+        <rng:zeroOrMore>
+          <rng:ref name="model.headLike"/>
+        </rng:zeroOrMore>
+        <rng:oneOrMore>
+          <rng:ref name="model.pLike"/>
+        </rng:oneOrMore>
       </content>
       <remarks>
         <p>This element is modelled on an element in the Text Encoding Initiative (TEI)
@@ -19869,27 +19869,32 @@
           they are required by software, or 2) multiple, alternative slurs are needed.</p>
       </remarks>
     </elementSpec>
-    <elementSpec ident="normalization" module="MEI.header">
-      <desc>Indicates the extent of normalization or regularization of the original source carried
-        out in converting it to electronic form.</desc>
+    <elementSpec ident="physDesc" module="MEI.header">
+      <desc>(physical description) – Container for information about the appearance, construction,
+        or handling of physical materials, such as their dimension, quantity, color, style, and
+        technique of creation.</desc>
       <classes>
         <memberOf key="att.common"/>
         <memberOf key="att.bibl"/>
-        <memberOf key="att.dataPointing"/>
-        <memberOf key="att.lang"/>
-        <memberOf key="att.regularMethod"/>
-        <memberOf key="model.editorialDeclPart"/>
       </classes>
       <content>
         <rng:zeroOrMore>
           <rng:ref name="model.headLike"/>
         </rng:zeroOrMore>
-        <rng:oneOrMore>
+        <rng:zeroOrMore>
           <rng:ref name="model.pLike"/>
-        </rng:oneOrMore>
+        </rng:zeroOrMore>
+        <rng:zeroOrMore>
+          <rng:ref name="model.physDescPart"/>
+        </rng:zeroOrMore>
       </content>
       <remarks>
-        <p>This element is modelled on an element in the Text Encoding Initiative (TEI)
+        <p>Dedicatory text and title page features may also be encoded here when they are not
+          transcribed as part of the front or back matter; i.e., when they are considered to be
+          meta-data rather than a transcription.</p>
+      </remarks>
+      <remarks>
+        <p>This element is modelled on an element in the Encoded Archival Description (EAD)
           standard.</p>
       </remarks>
     </elementSpec>