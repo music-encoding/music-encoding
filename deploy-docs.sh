db872ef789bd664e710bee139f4810a25a6d465b
Andrew Hankinson <andrew.hankinson@bodleian.ox.ac.uk>
2018-06-15 08:24:33 +0100

    Fixed: Update comments to reflect the actual process
    Also commented out the testing case.

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -2,23 +2,10 @@
 
 # This script builds the documentation and automatically commits
 # it to the 'guidelines' directory.
-# The directions for this are found here: https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
-# Note that for this to work it is essential for the same private/public key pair to
-# be available on both the music-encoding and guidelines directories. This means:
-# 1. Generate the public/private key: ssh-keygen -t rsa -b 4096 -C "andrew.hankinson@gmail.com"
-# 2. Rename the keys deploy_key and deploy_key.pub
-# 3. Using the travis CLI tool add these to both the guidelines and music-encoding travis
-#    builds as an encrypted key:
-#   - guidelines: travis encrypt-file -r music-encoding/guidelines deploy_key
-#   - music-encoding: travis encrypt-file -r music-encoding/music-encoding deploy_key
-#   Make note of the openssl line after adding the key to the music-encoding repo.
-# 4. Add the public key (deploy_key.pub) to both of these repositories as a deploy key.
-#    Be sure to grant it write access!
-# 5. For the music-encoding repo, take the openssl command mentioned when adding the key and
-#    replace the line in the script below with the new line.
-# 6. Ensure you update the .travis.yml file variable ENCRYPTION_LABEL with the identifier from that string, e.g.,
-#    with 'encrypted_20063e125f98_key' the identifier is 20063e125f98.
-
+# It uses an encrypted GH_TOKEN setting in Travis to check out the Guidelines, build them, commit
+# the changes, and then push.
+#
+# Changes to the major version of MEI should be reflected by updating the MEI_VERSION variable.
 
 set -e # Exit with nonzero exit code if anything fails
 MEI_VERSION="v3"
@@ -34,9 +21,9 @@ if [ "${TRAVIS_BRANCH}" == "develop" ]; then
 elif [ "${TRAVIS_BRANCH}" == "master" ]; then
     OUTPUT_FOLDER=${MEI_VERSION}
     DOCS_BRANCH="master"
-elif [ "${TRAVIS_BRANCH}" == "feature-build-with-travis" ]; then  # to be removed when merged to develop.
-    OUTPUT_FOLDER="dev"
-    DOCS_BRANCH="develop"
+#elif [ "${TRAVIS_BRANCH}" == "feature-build-with-travis" ]; then  # to be removed when merged to develop.
+#    OUTPUT_FOLDER="dev"
+#    DOCS_BRANCH="develop"
 else
     echo "Will not build docs for branch ${TRAVIS_BRANCH}"
     exit 0
