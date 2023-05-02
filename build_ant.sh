08a57ec683f5d06c0524fa2b91def748b9b2ff24
bwbohl <b.w.bohl@gmail.com>
2023-05-02 23:58:37 +0200

    build_ant.sh: remove deprecated lines

--- build_ant.sh
+++ build_ant.sh
@@ -6,6 +6,3 @@ ant reset
 echo "Building MEI Schemata and HTML Guidelines"
 echo "========================================="
 docker run --rm -it --name mei-ant-worker -v $(PWD):/opt/music-encoding ghcr.io/music-encoding/docker-mei:latest /opt/music-encoding/entrypoint_ant-worker.sh
-# in future use ghcr.io/bwbohl/docker-mei:latest ot corresponding repo if moved to music-encoding
-# alternatively use following line if you need to emulate thte plattform
-#docker run --platform=linux/amd64 --rm -it --name mei-ant-worker -v $(PWD):/opt/music-encoding ghcr.io/bwbohl/docker-mei:latest /opt/music-encoding/entrypoint_ant-worker.sh
