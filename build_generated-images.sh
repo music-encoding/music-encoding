68479c25c637261d39ab1575a3c4331abe1c1c15
bwbohl <b.w.bohl@gmail.com>
2023-05-02 23:54:40 +0200

    Update build_generated-images.sh
    use ghcr.io/music-encoding/docker-mei:latest to generate SVGs

--- build_generated-images.sh
+++ build_generated-images.sh
@@ -2,7 +2,7 @@
 echo ""
 echo "Generating Example Images from MEI"
 echo "=================================="
-docker run --rm -it -v $(pwd)/build/assets/images/GeneratedImages:/data edirom/staticverovioconverter:latest
+docker run --rm -it --name mei-verovio-worker -v $(PWD):/opt/music-encoding/music-encoding ghcr.io/music-encoding/docker-mei:latest /bin/bash -c "node index.js"
 # alternatively use following line if you need to emulate thte plattform
 #docker run --platform=linux/amd64 --rm -it -v $(pwd)/build/assets/images/GeneratedImages:/data edirom/staticverovioconverter:latest
 # in future use edirom/staticverovioconverter:latest
