cbcafb1d873bd36f27c72717e06cd2ed976bdd1b
bwbohl <b.w.bohl@gmail.com>
2023-05-03 02:18:29 +0200

    build.xml: update saxon to 11.4

--- build_ant.sh
+++ build_ant.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-echo ""
-echo "Purging old build artifacts and lib"
-echo "==================================="
-ant reset
 echo "Building MEI Schemata and HTML Guidelines"
 echo "========================================="
 docker run --rm -it --name mei-ant-worker -v $(PWD):/opt/music-encoding ghcr.io/music-encoding/docker-mei:latest /opt/music-encoding/entrypoint_ant-worker.sh
