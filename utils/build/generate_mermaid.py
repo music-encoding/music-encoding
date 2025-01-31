301df756ba123927ffd9c4eec4bfd3d13abb0f86
musicEnfanthen <stefan.muennich@unibas.ch>
2025-01-31 22:28:47 +0100

    fix(utils): run mermaid script on all module files

--- utils/build/generate_mermaid.py
+++ utils/build/generate_mermaid.py
@@ -11,59 +11,54 @@ def parse_xml(file_path):
 
 def find_spec_by_ident(roots, ident):
     spec_types = [
-        ("moduleSpec", "Module"),
-        ("macroSpec", "Macro"),
-        ("classSpec", "Class"),
-        ("elementSpec", "Element")
+        "moduleSpec",
+        "macroSpec",
+        "classSpec",
+        "elementSpec"
     ]
 
     for root in roots:
-        for spec_type, label in spec_types:
+        for spec_type in spec_types:
             for spec in root.findall(f".//{{http://www.tei-c.org/ns/1.0}}{spec_type}"):
                 spec_id = spec.get("ident")
                 if spec_id == ident:
-                    return spec, label
+                    return spec
     return None, None
 
 
 def generate_mermaid_graph(roots, ident):
+    def append_member_of_graph(graph, roots, ident):
+        spec = find_spec_by_ident(roots, ident)
+        if spec is not None:
+            spec_id = spec.get("ident")
+            print(f"Processing {spec_id}")
+
+            module_id = spec.get("module")
+            spec_node = f'{spec_id}["{spec_id} ({module_id})"]'
+            graph.append(spec_node)
+
+            for member_of in spec.findall(".//{http://www.tei-c.org/ns/1.0}memberOf"):
+                member_of_id = member_of.get("key")
+                member_of_edge = f"{member_of_id} --> {spec_id}"
+                graph.append(member_of_edge)
+
+                # Recursively append the graph for the member_of class
+                append_member_of_graph(graph, roots, member_of_id)
+
     graph = ["graph LR"]
-    found = False
+    append_member_of_graph(graph, roots, ident)
 
-    spec, label = find_spec_by_ident(roots, ident)
-
-    if spec is not None:
-        spec_id = spec.get("ident")
-        module_id = spec.get("module")
-        graph.append(f"{spec_id}[{label}: {spec_id}] --> {module_id}")
-        found = True
-
-        for member_of in spec.findall(".//{http://www.tei-c.org/ns/1.0}memberOf"):
-            member_of_id = member_of.get("key")
-            graph.append(f"{member_of_id}[Class: {member_of_id}] --> {spec_id}")
-
-            # Look up the member_of_id in all roots
-            member_of_spec, member_of_label = find_spec_by_ident(roots, member_of_id)
-            if member_of_spec is not None:
-                member_of_module_id = member_of_spec.get("module")
-                graph.append(
-                    f"{member_of_module_id} --> {member_of_id}[{member_of_label}: {member_of_id}]")
-
-    if not found:
-        graph.append(f"NotFound[Element: {ident} not found]")
+    # else:
+    #    graph.append(f"NotFound[Element: {ident} not found]")
 
     return "\n".join(graph)
 
 
 def generate_mermaid_graph_from_xml(ident):
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    file_paths = [
-        os.path.join(script_dir, "../../source/modules/MEI.cmn.xml"),
-        os.path.join(script_dir, "../../source/modules/MEI.shared.xml"),
-        os.path.join(script_dir, "../../source/modules/MEI.analytical.xml"),
-        os.path.join(script_dir, "../../source/modules/MEI.gestural.xml"),
-        os.path.join(script_dir, "../../source/modules/MEI.visual.xml")
-    ]
+    modules_dir = os.path.join(script_dir, "../../source/modules")
+    file_paths = [os.path.join(modules_dir, file)
+                  for file in os.listdir(modules_dir) if file.endswith(".xml")]
     roots = [parse_xml(file_path) for file_path in file_paths]
     mermaid_graph = generate_mermaid_graph(roots, ident)
 
